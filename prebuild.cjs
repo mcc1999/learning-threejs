5f6fb01d775766053a61a8687a866e1b9848da2e
Ma Chengcong <machengcong@mudu.tv>
2023-03-30 17:31:47 +0800

    feat: READE code link10

--- prebuild.cjs
+++ prebuild.cjs
@@ -139,7 +139,7 @@ function generateReadMeExamplesTreeString(tree, treeLevel = 1) {
     if(!tree[i].link){
       str = str + '  '.repeat(treeLevel) + `- ${tree[i].title}\n`
     }else {
-      str = str + '  '.repeat(treeLevel) + `- [${tree[i].title}](${tree[i].link})<a href=${tree[i].filepath}><span style="color: purple">【code】</span></a>\n`
+      str = str + '  '.repeat(treeLevel) + `- [${tree[i].title}](${tree[i].link})<span style="color: purple"><a href=${tree[i].filepath}>【code】</a></span>\n`
     }
     if(tree[i].child) {
       str = str +  generateReadMeExamplesTreeString(tree[i].child, treeLevel + 1) + `\n`
