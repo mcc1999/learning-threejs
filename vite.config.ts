24b5a60e437a99bdb0d6e10de6fe6002882423f9
Ma Chengcong <machengcong@mudu.tv>
2023-03-30 10:34:55 +0800

    test: folder -> docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 export default {
   build: {
+    base: './',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         '01-01': '/src/pages/01-01.html',
         '01-02': '/src/pages/01-02.html',
         // ...
-      }
-    }
+      },
+    },
   }
 }
