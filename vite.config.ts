99f450f53035a6f54732fe372ee89cb187ed3e39
Ma Chengcong <machengcong@mudu.tv>
2023-03-30 10:08:12 +0800

    2023.03.30: switch to gh-pages

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        '01-01': '/src/pages/01-01.html',
+        '01-02': '/src/pages/01-02.html',
+        // ...
+      }
+    }
+  }
+}
