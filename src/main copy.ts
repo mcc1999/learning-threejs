aad7bfd2f8d4a55fa8970f6635cb0f9341f18fde
Ma Chengcong <958625996@qq.com>
2023-04-23 18:32:38 +0800

    feat: css2DRenderer && css#dRenderer

--- src/main copy.ts
+++ src/main copy.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
+import { GUI } from 'dat.gui';
 
 import './style.css'
 /**
@@ -12,27 +13,26 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 30, 50)
 
-// 创建半径为2的球形几何体
+// 创建半径为5的球形几何体
 const sphere = new THREE.SphereGeometry(2, 50, 50);
 const sphereMaterial = new THREE.MeshBasicMaterial({
   color: 0xffff00,
 })
 const sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
-scene.add(sphereMesh);
 
-// 创建label div标筬
 const label = document.createElement('div');
 label.innerHTML = 'Hello Three.js';
-label.style.color = '#fff';
-label.style.backgroundColor = 'transparent';
+label.style.color = '#ffffff';
 const object = new CSS2DObject(label);
 
-sphereMesh.position.set(20, 20, 0);
-object.position.set(0, 3, 0);
 sphereMesh.add(object);
+scene.add(sphereMesh);
+sphereMesh.position.set(20, 20, 0);
+sphereMesh.castShadow = true;
+object.position.set(5 * 1.5, 0, 0);
 
 // 创建一个方块并能生成阴影
-const cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
+const cubeGeometry = new THREE.BoxGeometry(20, 10, 20);
 const cubeMaterial = new THREE.MeshStandardMaterial({
 	metalness: 0.6,
 	roughness: 0.1,
@@ -47,18 +47,26 @@ scene.add(cube);
 scene.add(plane);
 cube.castShadow = true;
 plane.receiveShadow = true;
-plane.lookAt(new THREE.Vector3(0, 1, 0));
+plane.rotation.x = -Math.PI / 2;
 cube.position.set(0, 5, 0);
 
 // 创建环境光
-const ambientLight = new THREE.AmbientLight(0x404040, 0.75);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
 // 创建点光源
 const pointLight = new THREE.PointLight(0xffff00, 0.5);
 pointLight.castShadow = true;
 pointLight.decay = 0
 pointLight.position.set(10, 30, 20);
-sphereMesh.add(pointLight);
+pointLight.shadow.mapSize.width = 512;
+pointLight.shadow.mapSize.height = 512;
+pointLight.shadow.camera.near = 0.5;
+pointLight.shadow.camera.far = 500;
+scene.add(pointLight);
+
+const gui = new GUI();
+// @ts-ignore
+gui.add(pointLight, 'distance', 0, 50, 0.1).name('distance')
 
 // 3. 创建网格辅助器
 const axesHelper = new THREE.AxesHelper(30);
@@ -67,14 +75,13 @@ scene.add( axesHelper );
 scene.add( gridHelper );
 
 // 4. 创建渲染器renderer并设置尺寸
-const css2Renderer = new CSS2DRenderer();
-document.body.appendChild( css2Renderer.domElement );
-css2Renderer.setSize( window.innerWidth, window.innerHeight );
-css2Renderer.domElement.style.position = 'absolute';
-css2Renderer.domElement.style.top = '0px';
-
+// const labelRenderer = new CSS2DRenderer();
+// labelRenderer.domElement.style.position = 'absolute';
+// labelRenderer.domElement.style.top = '0px';
+// document.body.appendChild( labelRenderer.domElement );
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // 渲染阴影
 renderer.physicallyCorrectLights = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -87,7 +94,7 @@ window.addEventListener('resize', () => {
  camera.updateProjectionMatrix()
 
  renderer.setSize(window.innerWidth, window.innerHeight)
- css2Renderer.setSize(window.innerWidth, window.innerHeight)
+//  labelRenderer.setSize(window.innerWidth, window.innerHeight)
 })
 
 const clock = new THREE.Clock(true);
@@ -95,9 +102,10 @@ const clock = new THREE.Clock(true);
 function animate() {
   // 设置了autoRotate / enableDamping = true， 需要在render函数中update()
   controls.update() 
-  sphereMesh.position.set(20 * Math.cos(clock.getElapsedTime()), 20, 20 * Math.sin(clock.getElapsedTime()));
+  sphereMesh.position.set(10 * Math.cos(clock.getElapsedTime()), 20, 20 * Math.sin(clock.getElapsedTime()));
+  pointLight.position.set(10 * Math.cos(clock.getElapsedTime()), 20, 20 * Math.sin(clock.getElapsedTime()));
   renderer.render( scene, camera );
-  css2Renderer.render( scene, camera );
+  // labelRenderer.render( scene, camera );
   requestAnimationFrame( animate );
 }
 animate();
---
+++ src/markdown/05-renderer.md
@@ -0,0 +1,14 @@
+# Renderer
+
+- WebGLRender
+- CSS3DRender
+- CSS2DRender
+
+## WebGLRender
+Most Powerful 3D Render
+
+## CSS3DRender
+CSS 3D 通过CSS3的transform属性， 将层级的3D变换应用到DOM元素上，also provides hardware accelerated rendering, but only has support for a limited set of Three.js features
+
+## CSS2DRender
+将三维物体和基于HTML的标签相结合，则这一渲染器将十分有用。在这里，各个DOM元素也被包含到一个CSS2DObject实例中，并被添加到场景图中
